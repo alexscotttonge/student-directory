4ca5dcfef3efd56bc9b6966851890dea938e5400
Alex <alex@scotttonge.com>
2017-09-13 09:08:30 +0100

    Exercise 8.2: only print students whose first names start with A-N

--- directory.rb
+++ directory.rb
@@ -39,8 +39,10 @@ def print_header
 end
 
 def print(students)
-  students.each.with_index(1) do |student, index|
-    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
+    students.each.with_index(1) do |student|
+      if student[:name].match /^[A-Na-n]/
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
