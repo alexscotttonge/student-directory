0e1eceb35dc53d89674da78f036c73939f0a0f27
Alex <alex@scotttonge.com>
2017-09-15 15:23:51 +0100

    Revert "Exercise 14.3: refactored code and added new methods"
    This reverts commit f521d967353241e72f6504fc342c107aafe90246.

--- directory.rb
+++ directory.rb
@@ -1,5 +1,3 @@
-
-
 # students = [
 #    {name: "Dr. Hannibal Lecter", cohort: :november},
 #    {name: "Darth Vader", cohort: :november},
@@ -56,13 +54,17 @@ end
 
 
 def input_students
-# removed @students array
+
+  # create an empty array
+  @students = []
+
   puts 'Please enter the names of the students'
 
   # get the full name
   @name = STDIN.gets.chomp
 
-  loop do # changed while loop to simple loop
+  # force user to input name so empty list won't be created
+  while true do
     if @name.empty?
       puts "Please enter a name"
       @name = STDIN.gets.chomp
@@ -76,8 +78,14 @@ def input_students
 
   # while name is not empty, repeat this code
   while !@name.empty? do
-    add_students_to_array # add the student hash to the array
-    count_students
+    # add the student hash to the array
+    add_students_to_array
+
+    if @students.length > 1
+      puts "Now we have #{@students.count} students"
+    else
+      puts "Now we have #{@students.count} student"
+    end
     # get another name from the user
     puts "Please enter another name and cohort."
     @name = STDIN.gets.chomp
@@ -86,15 +94,6 @@ def input_students
   # return array of students
   @students
 end
-
-def count_students # added separate method to count students
-  if @students.length > 1
-    puts "Now we have #{@students.count} students"
-  else
-    puts "Now we have #{@students.count} student"
-  end
-end
-
 
 def add_students_to_array
   @students << {name: @name, cohort: @cohort}
@@ -145,7 +144,7 @@ def load_students(filename = "students.csv")
 end
 
 def try_load_students
-  filename = ARGV.first # first argument from the command line
+  filename = "students.csv" # first argument from the command line
   return if filename.nil? # get out of the method if it isn't given
   if File.exists?(filename) # if it exists
     load_students(filename)
